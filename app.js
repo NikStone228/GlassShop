4d35e5cdc8c89730cb2012548aedbb18074a414c
Timur <kandareli1995@gmail.com>
2021-01-13 18:33:26 +0300

    create carcass

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const path = require('path')
+const indexRouter = require('./routes/indexRouter')
+
+const app = express()
+
+// Handlebars
+app.set('views', 'views')
+app.set('view engine', 'hbs')
+
+// Static & Parser
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Router
+app.use('/', indexRouter)
+
+
+
+app.listen(3000, () => {
+  console.log('Server has been started on port 3000')
+})
