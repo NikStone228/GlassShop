4cf0a4d46d1a848afb570162f2cd7047e044d566
NikStone228 <nikitosvanilla@gmail.com>
2021-01-14 17:00:58 +0400

    block commit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,32 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-router.get('/', async (req, res) => {
-  res.render('index')
-})
+router.get("/", async (req, res) => {
+  res.render("index");
+});
 
-module.exports = router
+router.get("/shower", (req, res) => {
+  res.render("shower");
+});
+
+router.get("/doors", (req, res) => {
+  res.render("doors");
+});
+
+router.get("/mirror", (req, res) => {
+  res.render("mirror");
+});
+
+router.get("/euro-mirror", (req, res) => {
+  res.render("euro-mirror");
+});
+
+router.get("/shelves", (req, res) => {
+  res.render("shelves");
+});
+
+router.get("/skinali", (req, res) => {
+  res.render("skinali");
+});
+
+module.exports = router;
