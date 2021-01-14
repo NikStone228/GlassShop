7f2121b2c18f79cebfeb0df68cc2cd8734889508
NikStone228 <nikitosvanilla@gmail.com>
2021-01-14 15:47:28 +0400

    verstka

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const path = require('path')
-const indexRouter = require('./routes/indexRouter')
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const path = require("path");
+const indexRouter = require("./routes/indexRouter");
 
-const app = express()
+const app = express();
 
 // MongoDB
-mongoose.connect('mongodb+srv://glassShop:akP7zAugnGTwJVch@cluster0.cxklx.mongodb.net/glassShop?retryWrites=true&w=majority', {
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useNewUrlParser: true,
-  useFindAndUpdate: false
-})
+mongoose.connect(
+  "mongodb+srv://glassShop:akP7zAugnGTwJVch@cluster0.cxklx.mongodb.net/glassShop?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useFindAndUpdate: false,
+  }
+);
 
 // Handlebars
-app.set('views', 'views')
-app.set('view engine', 'hbs')
+app.set("views", "views");
+app.set("view engine", "hbs");
 
 // Static & Parser
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // Router
-app.use('/', indexRouter)
-
-
+app.use("/", indexRouter);
 
 app.listen(3000, () => {
-  console.log('Server has been started on port 3000')
-})
+  console.log("Server has been started on port 3000");
+});
 
-const password = 'akP7zAugnGTwJVch'
+const password = "akP7zAugnGTwJVch";
