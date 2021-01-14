2693b8f577bc700810b41a9534b8c63783e68d76
NikStone228 <nikitosvanilla@gmail.com>
2021-01-14 19:14:30 +0400

    maps added

--- server/app.js
+++ server/app.js
@@ -1,53 +1,54 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const path = require('path')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const hbs = require("hbs");
+const path = require("path");
+const app = express();
 
-const indexRouter = require('./routes/indexRouter')
-const imgUploadRouter = require('./routes/imgUpload')
-const yandexApiRouter = require('./routes/yandexApi')
+const indexRouter = require("./routes/indexRouter");
+const imgUploadRouter = require("./routes/imgUpload");
+const yandexApiRouter = require("./routes/yandexApi");
 
-app.use(cookieParser())
+app.use(cookieParser());
 // Session
-app.use(session({
-  secret: 'glass glass shop',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false },
-}))
+app.use(
+  session({
+    secret: "glass glass shop",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 app.use((req, res, next) => {
   res.locals.isAuth = req.session.isAuthenticated; // записываем в локалс имя юзера из сессии
   next();
 });
 
-
 // MongoDB
 mongoose.connect(process.env.MONGODB, {
   useUnifiedTopology: true,
   useFindAndModify: true,
   useNewUrlParser: true,
-  useFindAndUpdate: true
-})
+  useFindAndUpdate: true,
+});
 
 // Handlebars
-app.set('views', 'views')
-app.set('view engine', 'hbs')
+app.set("views", "views");
+app.set("view engine", "hbs");
 
 // Static & Parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Router
-app.use('/', indexRouter)
-app.use('/upload', imgUploadRouter)
-app.use('/api', yandexApiRouter)
-
+app.use("/", indexRouter);
+app.use("/upload", imgUploadRouter);
+app.use("/api", yandexApiRouter);
 
 app.listen(3000, () => {
-  console.log('Server has been started on port 3000')
-})
+  console.log("Server has been started on port 3000");
+});
